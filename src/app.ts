93261fe1b08d1fc0860b01fe2cde22443c5b553b
johnvesslyalti <altijohnvessly@gmail.com>
2025-12-23 09:03:31 +0530

    fix: move the authentication to the like routes

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(express.json())
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/posts", postRoutes);
-app.use("/api/v1/likes", auth.verifyAccessToken, likeRoutes);
+app.use("/api/v1/likes", likeRoutes);
 app.use("/api/v1/comments", auth.verifyAccessToken, commentRoutes);
 app.use("/api/v1/follow", followRoutes)
 app.use("/api/v1/feed", auth.verifyAccessToken, feedRoutes);
--- src/modules/like/like.routes.ts
+++ src/modules/like/like.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { likeController } from "./like.controller";
+import { auth } from "../../middlewares/auth";
 
 const router = Router();
 
-router.post("/", likeController.like);
-router.delete("/", likeController.unlike);
+router.post("/", auth.verifyAccessToken, likeController.like);
+router.delete("/", auth.verifyAccessToken, likeController.unlike);
 router.get("/", likeController.count)
 
 export default router;
