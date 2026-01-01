901d033d5d9da1ed8adbfab766d86a141c6b7958
johnvesslyalti <altijohnvessly@gmail.com>
2026-01-01 14:18:18 +0530

    fix: add the block module in the app and changed the routes of the like and comment

--- src/modules/like/like.routes.ts
+++ src/modules/like/like.routes.ts
@@ -4,8 +4,8 @@ import { auth } from "../../middlewares/auth";
 
 const router = Router();
 
-router.post("/", auth.verifyAccessToken, likeController.like);
-router.delete("/", auth.verifyAccessToken, likeController.unlike);
-router.get("/", likeController.count)
+router.post("/:id", auth.verifyAccessToken, likeController.like);
+router.delete("/:id", auth.verifyAccessToken, likeController.unlike);
+router.get("/:id", likeController.count)
 
 export default router;
