a58e4ddccf0bd994dcad8ef278975b127c215854
johnvesslyalti <altijohnvessly@gmail.com>
2026-02-10 15:29:19 +0530

    feat: add global validation pipe for request validation.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ConfigService } from "@nestjs/config";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import * as cookieParser from "cookie-parser";
+import { ValidationPipe } from "./common/pipes/validation.pipe";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,8 +13,7 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
-
-
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get("PORT") || 3001);
