61a9861351c8e6c8a9e6543d130e4a9c23ecc0c3
johnvesslyalti <altijohnvessly@gmail.com>
2026-02-11 15:22:43 +0530

    feat: Implement Redis caching for profile data, refactor profile service to use a dedicated repository, and add integration tests.

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ProfileService } from "./profile.service";
 import { ProfileController } from "./profile.controller";
+import { RedisCacheModule } from "../common/cache/cache.module";
+import { ProfileRepository } from "./profile.repository";
 
 @Module({
+  imports: [RedisCacheModule],
   controllers: [ProfileController],
-  providers: [ProfileService],
+  providers: [ProfileService, ProfileRepository],
 })
 export class ProfileModule {}
