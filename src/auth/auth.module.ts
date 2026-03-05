e23d6ba3d0a558dc2ec2eddeffdc944a862cfa7a
johnvesslyalti <altijohnvessly@gmail.com>
2026-03-05 15:24:40 +0530

    feat: Implement Google OAuth authentication with new strategy, controller endpoints, service logic, and database schema updates.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from "./auth.controller";
 import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
 import { JwtStrategy } from "./strategies/jwt.strategy";
+import { GoogleStrategy } from "./strategies/google.strategy";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { EmailModule } from "../email/email.module";
 
@@ -22,7 +23,7 @@ import { EmailModule } from "../email/email.module";
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, GoogleStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
