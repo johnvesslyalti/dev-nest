6c60a6ac9f2b1c51bc2872574a5208dd38e9859d
johnvesslyalti <altijohnvessly@gmail.com>
2026-03-29 14:15:43 +0530

    chore: remove frontend directory and associated configuration files

--- frontend/src/api/modules/comments.ts
+++
@@ -1,6 +0,0 @@
-import client from '../client';
-
-export const commentApi = {
-  create: (postId: string, content: string) => client.post(`/posts/${postId}/comments`, { content }),
-  getByPost: (postId: string) => client.get(`/posts/${postId}/comments`),
-};
