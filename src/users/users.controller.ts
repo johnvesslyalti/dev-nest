d35dc6ee6d752b65dc711df66af2dceff618b595
johnvesslyalti <altijohnvessly@gmail.com>
2026-04-23 09:54:12 +0530

    refactor: remove create user functionality and associated DTO from users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,19 +4,12 @@ import {
   Param,
   UseGuards,
   UseInterceptors,
-  UsePipes,
-  Post,
-  Body,
 } from "@nestjs/common";
 import { UserService } from "./users.service";
 import { ApiKeyGuard } from "../common/guards/api-key.guard";
 import { TransformInterceptor } from "../common/interceptors/transform.interceptor";
-import { ValidationPipe } from "../common/pipes/validation.pipe";
-import { IsString, IsNotEmpty } from "class-validator";
 import { ThrottlerGuard } from "@nestjs/throttler";
 import { CacheInterceptor, CacheTTL } from "@nestjs/cache-manager";
-
-import { CreateUserDto } from "./dto/create-user.dto";
 
 @Controller("users")
 @UseGuards(ApiKeyGuard, ThrottlerGuard)
@@ -29,17 +22,5 @@ export class UsersController {
   @Get(":id")
   async getUser(@Param("id") id: string) {
     return this.userService.findById(id);
-  }
-
-  @Post()
-  @UsePipes(new ValidationPipe())
-  // Using the custom pipe we created.
-  // Note: NestJS has built-in ValidationPipe, but we are using our custom '../common/pipes/validation.pipe'
-  async createUser(@Body() createUserDto: CreateUserDto) {
-    // This is just a mock because we haven't implemented create in service yet fully (or we did just pass through)
-    // But for architecture demo:
-    // Controller -> Pipe (validation) -> Service -> Repository
-    // Interceptor (transform response) -> Response
-    return { message: "User created (mock)", data: createUserDto };
   }
 }
